7d4db4eb6748d88d8315df521eefb25b01289aed
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-05 18:02:38 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - task number five
+ *
+ * Description: A C program that print with printf
+ *
+ * Return: 0 always is (successful)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

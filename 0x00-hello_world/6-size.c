4ce622aa21a69c58ff396c4dc9841e9e18edfb9d
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-05 18:04:25 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - The master function in C program
+ *
+ *Description: A C program that print the size of th data types
+ *
+ *Return: 0 always mean (success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}

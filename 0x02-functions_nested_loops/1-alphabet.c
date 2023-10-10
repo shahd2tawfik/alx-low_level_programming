c1251486d34a09318937871caf62339376def6ee
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-10 14:47:51 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - this is the master function
+ *
+ * Description: write alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void){
+  char c;
+
+  c = 'a';
+
+  while(c <= 'z'){
+    putchar(c);
+    c++;
+  }
+  putchar('\n');
+  return(0);
+}
+ 

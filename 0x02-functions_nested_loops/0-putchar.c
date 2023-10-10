cb45ee81242debf58b746bb4bf67f44e37944d78
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-10 14:33:00 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - master function
+ *
+ * Description: print given output
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+}

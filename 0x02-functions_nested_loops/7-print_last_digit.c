6f37451958f0786cd1ef8ea9c5fc2fe58e512f46
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-11 16:04:17 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,20 +9,14 @@
 
 int print_last_digit(int n)
 {
-int a;
-
 if (n < 0)
 {
-n = -n;
-
-a = n % 10;
+n = (n % 10) * -1;
 }
-if (a < 0)
-{  
-a = -a;
-
-_putchar(a + '0')
-
+else
+{
+n = n % 10;
+}
+_putchar(a + '0');
 return (a);
-}
 }

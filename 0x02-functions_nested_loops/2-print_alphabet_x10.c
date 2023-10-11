4aed4a395c48e95e814f6e3833189b7055bf391b
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-11 07:57:48 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - this is the master function
+ *
+ * Description: enable us to write alphabet 10 times
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+char c;
+int i = 1;
+
+while (i <= 10)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+i++;
+}

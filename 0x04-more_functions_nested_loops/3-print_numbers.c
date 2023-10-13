58a3df77d79de5ce901127dc9b3f58a8f4539cc8
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-13 15:59:03 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - check if the input is upper of not
+ *
+ * Description: this function print the digits
+ *
+ * Return: 0 if the input is upper and 1 if it's not
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+{
+	_putchar(i + '0');
+}
+	_putchar('\n');
+}

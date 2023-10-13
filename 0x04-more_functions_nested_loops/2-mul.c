91552b4cdfa2cbee4bf4f9f87acfe4941361f038
shahd2tawfik <shahdahmed232004@gmail.com>
2023-10-13 15:55:22 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: check the input1
+ *
+ * @b: check the input2
+ *
+ * Return: 0 if the input is upper and 1 if it's not
+*/
+
+
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}

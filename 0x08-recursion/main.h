980065f390ee0fce67f9c3d9c81d1596dc2321d6
shahd2tawfik <shahdahmed232004@gmail.com>
2023-11-03 23:12:44 +0200

    first commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,12 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
 
 #endif

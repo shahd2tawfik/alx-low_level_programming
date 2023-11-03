bd1ffce5ac124080e9e1eb8b4582f0fb0921ee7e
shahd2tawfik <shahdahmed232004@gmail.com>
2023-11-03 23:52:13 +0200

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,9 +4,13 @@
 * _sqrt_recursion - Calculate the natural square root of a number.
 *
 * @n: Number to calculate it natural square.
+* @i: Number to check if it is the square number
 *
 * Return: Natural square of n, or -1 if n does not have a natural square.
 */
+
+int check_sqrt(int n, int i);
+
 int _sqrt_recursion(int n)
 {
 	return (check_sqrt(n, 1));

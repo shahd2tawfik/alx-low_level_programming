ceac4b20c9eeaf1314495f8ccd8d88a4c8e50f2a
shahd2tawfik <shahdahmed232004@gmail.com>
2023-11-03 23:58:27 +0200

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,6 +7,9 @@
  *
  * Return: 1 if the string is palindrome and 0 if not.
  */
+
+int check_palindrome(char *s, int start, int end);
+
 int is_palindrome(char *s)
 {
     return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
